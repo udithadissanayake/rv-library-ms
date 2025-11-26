57c6eb3ec1a39e42cdad81dd3b52d45191abe45b
udithadissanayake <udithadissanayake@gmail.com>
2025-11-26 20:34:31 +0530

    first commit

---
+++ src/test/java/com/rv/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rv.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
